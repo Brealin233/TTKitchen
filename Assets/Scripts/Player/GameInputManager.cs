7d49c1e452d7860b4b42a1d1d342c41432f1282a
Brealin233 <1138061234@qq.com>
2023-04-19 16:22:19 +0800

    Clear Counter - Interact Action, C# Events

--- Assets/Scripts/Player/GameInputManager.cs
+++ Assets/Scripts/Player/GameInputManager.cs
@@ -2,16 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class GameInputManager : MonoBehaviour
 {
+   public event EventHandler inputInteractHandler;
+   
    private InputManager inputManager;
 
    private void Awake()
    {
       inputManager = new InputManager();
       inputManager.Player.Enable();
+      
+      inputManager.Player.Interact.performed += InteractOnPerFormed;   
+   }
+
+   private void InteractOnPerFormed(InputAction.CallbackContext obj)
+   {
+      inputInteractHandler?.Invoke(this, EventArgs.Empty);
    }
 
    public Vector2 GetMovementVectorNormalized()
